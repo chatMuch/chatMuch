3c932b007d5d4c51fae180729d2a6b3d92d7b9d5
dionjwa1 <dionjwa@gmail.com>
2021-07-12 17:30:01 -0700

    updated to include 2 server codes. Will seperate them into 2 files: app and socket

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 'use strict';
 
 // // 3rd Party Resources
-// const express = require('express');
-// const cors = require('cors');
-// const morgan = require('morgan');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-// // Esoteric Resources
-// const errorHandler = require('./error-handlers/500.js');
-// const notFound = require('./error-handlers/404.js');
-// const authRoutes = require('./routes/auth/routes.js');
-// const v2Routes = require('./routes/v2');
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./routes/auth/routes.js');
+const v2Routes = require('./routes/v2');
 
 const MessageQueue = require('./queue-server.js')
 const io = require('socket.io');
@@ -26,7 +26,7 @@ caps.on('connection', (socket) => {
     const p_user = join_User(socket.id, username, roomname);
     console.log(socket.id, "=id");
     socket.join(p_user.room);
-  }:
+  };
   
     socket.on('sent', (data) =>{
       console.log(data);
@@ -38,11 +38,45 @@ caps.on('connection', (socket) => {
     console.log(data);
     server.emit('delivered', data);
   });
+});
 
-  socket.on('in-transit', (data) => {
-    console.log(data);
-    server.emit('in-transit', data);
-  });
+socket.on(MessageQueue, (messages) => {
+  console.log(data);
+  server.emit(MessageQueue, messages);
+});
+
+})
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(authRoutes);
+app.use('/api/v2', v2Routes);
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
+
+// Export
+module.exports = {
+  server: app,
+  start: (port) => {
+    app.listen(port, () => {
+      console.log(`Server Up on ${port}`);
+    });
+  },
+};
+  // Potential Timeout or failed to deliver function
+  // socket.on('in-transit', (data) => {
+  //   console.log(data);
+  //   server.emit('in-transit', data);
+  // });
 
   // socket.on('received', (data) => {
   //   console.log(data);
@@ -55,11 +89,3 @@ caps.on('connection', (socket) => {
   //     // socket.broadcast.emit[o]
   //     console.log(obtain[i], 'Got It');
   //   }
-});
-
-socket.on(MessageQueue, (messages) => {
-  console.log(data);
-  server.emit(MessageQueue, messages);
-});
-
-})
