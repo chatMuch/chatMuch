0f7ff5f2c3ade95d10bea61f1d4d011e6b6dbb24
dionjwa1 <dionjwa@gmail.com>
2021-07-13 16:04:12 -0700

    merged socket and express servers

---
+++ src/server.js
@@ -0,0 +1,67 @@
+'use strict';
+
+//express server
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {});
+
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./routes/auth/routes.js');
+const v2Routes = require('./routes/v2');
+
+//socketServer
+const chat = io.of('/chat');
+
+chat.on('connection', (socket) => {
+  //room message of user joining room
+  socket.on('joinRoom', ({ username, roomname }) => {
+    //* create user;
+    server.emit('joinedRoom', `${username} has joined the ${roomname}`);
+  });
+  //chat message sent;
+  socket.on('chat', (data) => {
+    console.log(data);
+    server.emit('chat', data);
+  });
+
+
+  // user message when leaving room;
+  socket.on('disconnect', (id) => {
+    console.log(id);
+    server.emit(`${id} has left the room`);
+  });
+
+});
+// app Level MW
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(authRoutes);
+app.use('/api/v2', v2Routes);
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
+
+// Export
+module.exports = {
+  server: httpServer,
+  start: (port) => {
+    httpServer.listen(port, () => {
+      console.log(`Server Up on ${port}`);
+    });
+  },
+};
+
+//removed code...delete if not used.;
+// socket.on('delivered', (data) => {
+//   console.log(data);
+//   server.emit('delivered', data);
+// });
