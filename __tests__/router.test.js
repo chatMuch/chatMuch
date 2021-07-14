9770d8fcdfc6034cf6d7cc685a148975c547af5a
Tyler Johnson <tyler@csdconsultants.us>
2021-07-14 12:06:21 -0700

    adds test for post and put

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -14,13 +14,14 @@ afterAll(async () => {
   await db.drop();
 });
 
-describe('testing user things', () => {
+describe('testing routes', () => {
+
   const testUser = {
     username: 'testUser',
     password: 'password',
     role: 'salesPerson',
     token: '',
-    // id: 1,
+    id: 1,
   };
 
   const testCustomer = {
@@ -33,11 +34,12 @@ describe('testing user things', () => {
 
   };
 
+
   // Test POST/signup request
   test('testing POST request to create a new user', async () => {
     let response = await request.post('/signup').send(testUser);
 
-    console.log('this is the response.body, ', response.body.user);
+    // console.log('this is the response.body, ', response.body.user);
 
     expect(response.status).toEqual(201);
     expect(response.body.user.username).toEqual('testUser');
@@ -52,7 +54,7 @@ describe('testing user things', () => {
     const response = await request.post('/signin').auth(testUser.username, testUser.password);
 
     const userObject = response.body.user;
-    console.log('this is the userObject', userObject);
+    // console.log('this is the userObject', userObject);
 
     testUser.token = userObject.token;
 
@@ -69,25 +71,47 @@ describe('testing user things', () => {
     const response = await request.get('/users')
       .auth(testUser.token, { type: 'bearer' });
 
-    console.log('this is the userObject.token', testUser.token);
+    // console.log('this is the userObject.token', testUser.token);
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(['testUser']);
   });
 
-  
-  // can GET to all customers associated with a sales person
-  test('can GET to /api/v2/customers associated with a sales person', async () => {
-    const response = await request.post('/api/v2/customers/1').send(testCustomer);
+
+  // can POST a customer and GET all customers associated with a sales person
+  test('can GET and POST to /api/v2/customers associated with a sales person', async () => {
+    const newCustomer = await request.post('/api/v2/customers').auth(testUser.token, { type: 'bearer'}).send(testCustomer);
+    
+    console.log('newCustomer', newCustomer.body);
+
+    //this is getting all accounts associated with salesPerson id: 1
+    const response = await request.get('/api/v2/customers/1').auth(testUser.token, { type: 'bearer'});
+
+    console.log('this is the response.body for api/v2/customers', response.body);
 
 
-    console.log('this is the response.body', response.body);
     expect(response.status).toBe(200);
-    expect(response.body.id).toEqual(1);
-    expect(response.body.salesPerson).toEqual(1);
-    expect(response.body.name).toEqual('testCustomer');
-    expect(response.body.jobTitle).toEqual('VP of Perrier');
+    expect(response.body[0].id).toEqual(1);
+    expect(response.body[0].salesPerson).toEqual(1);
+    expect(response.body[0].name).toEqual('testCustomer');
+    expect(response.body[0].jobTitle).toEqual('VP of Perrier');
 
   });
+
+
+  test('can update an existing customer', async () => {
+    testCustomer.jobTitle = 'VP of San Pellegrino';
+
+    const response = await request.put('/api/v2/customers/1').auth(testUser.token, { type: 'bearer'}).send(testCustomer);
+    
+    console.log(testCustomer);
+    // console.log(response);
+
+
+    expect(response.status).toBe(202);
+    expect(response.body.jobTitle).toEqual('VP of San Pellegrino');
+
+  });
+
 
 });
